5887a8562e846cc452df0add911c01012bdb8cdd
Clamator <kalmah23@gmail.com>
2021-11-29 08:15:27 +0300

    add new finction in class

--- class.py
+++ class.py
@@ -1,7 +1,10 @@
 class Radio:
     def __init__(self):
         self.turn_on = False
+        self.station = 'Di FM'
 
     def Turn_on(self):
         self.turn_on = True
 
+    def change_the_wave(self):
+        self.station = 'Di FM2'
